4252282eef27cd6c2f23f233b3b18179b2c42251
anirudh <hegdeanirudh2003@gmail.com>
2024-05-21 22:13:27 +0530

    altered files

--- gscrape.py
+++ gscrape.py
@@ -1,6 +1,7 @@
 # This script serves as an example of how to use Python
 # & Playwright to scrape/extract data from Google Maps
 
+# playwright install
 from playwright.sync_api import sync_playwright
 from dataclasses import dataclass, asdict, field
 import pandas as pd
@@ -28,10 +29,8 @@ class Business:
 
 @dataclass
 class BusinessList:
-
     business_list: list[Business] = field(default_factory=list)
     save_at = 'output'
-
 
     def dataframe(self):
 
@@ -46,7 +45,6 @@ class BusinessList:
         self.dataframe().to_excel(f"output/{filename}.xlsx", index=False)
 
     def save_to_csv(self, filename):
-
 
         if not os.path.exists(self.save_at):
             os.makedirs(self.save_at)
@@ -77,9 +75,7 @@ def main():
         total = 1_000_000
 
     if not args.search:
-        #########
         # input #
-        #########
         categories = ["restaurant", "hotel", "pharmacy", "gym", "bank"]
         search_list = categories
 
@@ -91,7 +87,7 @@ def main():
         # Loop through specified categories
         for search_for_index, search_for in enumerate(search_list):
             print(f"-----\n{search_for_index} - {search_for}".strip())
-            page.goto("https://www.google.com/maps", timeout=60000)
+            page.goto("https://www.google.com/maps", timeout=600000)
             page.wait_for_timeout(5000)
 
             page.locator('//input[@id="searchboxinput"]').fill(search_for)
@@ -102,7 +98,6 @@ def main():
 
             # Scrolling📜
             page.hover('//a[contains(@href, "https://www.google.com/maps/place")]')
-
 
             previously_counted = 0
             index = 0
@@ -162,37 +157,7 @@ def main():
                     category_xpath = '//*[@id="QA0Szd"]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/span/span/button'
                     reviews_span_xpath = '//span[@aria-hidden="true"]/text()'
 
-                    # reviews_span_xpath = f'(//div[contains(@class, "F7nice")])[{index + 1}]//span[@aria-hidden="true"]'
-                    # reviews_span_xpath = '//div[contains(@class, "section-review-content")]'
-                    #
-
-                    # reviews_span_xpath = "//div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/span[1]/span[1]"
-                    # reviews_span_xpath = '//span[contains(@class, "section-rating")]/span[contains(@class, "section-star-display")] | /html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/span[1]/span[1]'
-                    # reviews_span_xpath = f'(//div[contains(@class, "F7nice")])[{index + 1}]'
-
-                    # name_xpath = '//div[contains(@class, "fontHeadlineSmall")]'
-                    # name_xpath = '//h1[@data-section-id="title"]'
-                    # address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
-                    # website_xpath = '//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
-                    # phone_number_xpath = '//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
-                    # reviews_span_xpath = '//span[@role="img"]'
-
-                    # name_xpath = '//h1[@data-section-id="title"]'
-                    # reviews_count_xpath = '//button[contains(@aria-label, "reviews")]/span[contains(@class, "gm2-caption")]'
-
-                    # name_xpath = '//div[contains(@class, "fontHeadlineSmall")]'
-                    # address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
-                    # website_xpath = '//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
-                    # phone_number_xpath = '//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
-                    # reviews_span_xpath = '//span[@role="img"]'
-                    # reviews_count_xpath = '//button[contains(@aria-label, "reviews")]/span[contains(@class, "gm2-caption")]'
-
                     business = Business()
-
-                    # if page.locator(name_xpath).count() > 0:
-                    #     business.name = page.locator(name_xpath).all()[0].inner_text()
-                    # else:
-                    #     business.name = ""
 
                     if page.locator(name_xpath).count() > 0:
                         business.name = page.locator(name_xpath).all()[0].inner_text()
@@ -211,11 +176,6 @@ def main():
                         business.phone_number = page.locator(phone_number_xpath).all()[0].inner_text()
                     else:
                         business.phone_number = ""
-                    # if page.locator(reviews_count_xpath).count() > 0:
-                    #     reviews_count_text = page.locator(reviews_count_xpath).all()[0].inner_text().strip()
-                    #     business.reviews_count = int(reviews_count_text.replace(',', ''))
-                    # else:
-                    #     business.reviews_count = 0
 
                     if listing.locator(reviews_span_xpath).count() > 0:
                         business.reviews_average = float(
@@ -236,15 +196,7 @@ def main():
 
                     business.latitude, business.longitude = extract_coordinates_from_url(page.url)
 
-                    # Extract additional GMB details
-                    # try:
-                    #     hours_xpath = '//*[@class="section-open-hours-container"]'
-                    #     if page.locator(hours_xpath).count() > 0:
-                    #         business.hours = page.locator(hours_xpath).all()[0].inner_text()
-                    # except Exception as e:
-                    #     print(f'Error extracting hours: {e}')
-
-                    business.category = search_for  # Set the category
+                    business.category = search_for
 
                     business_list.business_list.append(business)
                 except Exception as e:
